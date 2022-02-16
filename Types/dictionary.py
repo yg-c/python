d9d18100273459bafe62d29f071cc90acb300fbb
yann <yann.cla@gmail.com>
2022-02-16 17:13:28 +0100

    Create dictionary.py

---
+++ Types/dictionary.py
@@ -0,0 +1,12 @@
+# A Dictionary is a collection which is ordered and changeable. No duplicate members.
+
+thisdict =	{
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+} 
+
+print(thisdict)
+
+print(thisdict["brand"])
