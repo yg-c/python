cf6be5b6cc8747082af0df0f67e939e0225ffafc
yann <yann.cla@gmail.com>
2022-02-16 19:03:30 +0100

    Update tuple.py

--- Types/tuple.py
+++ Types/tuple.py
@@ -1,9 +1,30 @@
 # A tuple is a collection which is ordered and unchangeable. Allows duplicate members.
 
-x = ('apple', 123, 'cherry')
+""" Create """
+a_tuple = ("apple", "banana", "cherry", "apple", "cherry")
 
-print(x)
 
-print(x[1])
+""" Access """
+a_tuple[2] # Specific item
 
-print(len(x))
+a_tuple[-1] # Last item
+
+a_tuple[2:5] # Range
+
+if 'cherry' in a_tuple : # In the list
+	print('yes')
+
+
+""" Iterate """
+for t in a_tuple :
+	print(t)
+
+for i in range(len(a_tuple)):
+	print(a_tuple[i])
+
+
+""" Join """
+tuple1 = ('a', 'b', 'c')
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
