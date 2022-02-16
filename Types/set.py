9690a96ada24123cb025869e11d35f95b95dc07f
yann <yann.cla@gmail.com>
2022-02-16 17:13:32 +0100

    Create set.py

---
+++ Types/set.py
@@ -0,0 +1,7 @@
+# A Set is a collection which is unordered, unchangeable and unindexed. No duplicate members.
+
+x = {"apple", "banana", "cherry"}
+
+# The set() Constuctor
+
+thisset = set(("apple", "banana", "cherry"))
