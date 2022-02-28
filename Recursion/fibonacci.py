53cc6d919e5bb97edbec5996762cd9be550d76d9
yann <yann.cla@gmail.com>
2022-02-28 18:34:02 +0100

    Update fibonacci.py

--- Recursion/fibonacci.py
+++ Recursion/fibonacci.py
@@ -10,22 +10,3 @@ def fib(n):
 
 
 fib(4)
-
-
-# Memoization 
-
-numbs = {}
-
-def fibm(n):
-	if n <= 2:
-		return 1
-
-	if n in numbs:
-		return numbs[n]
-	else:
-		num = fibm(n-1) + fibm(n-2)
-		numbs[n] = num
-		return num
-
-
-print(fibm(39))
