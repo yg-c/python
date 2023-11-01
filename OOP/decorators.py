ed1446d557261193d0e2fb1e690c1b5371b7532d
Yann <yann.cla@gmail.com>
2023-11-01 09:50:22 +0100

    Update decorators.py

--- OOP/decorators.py
+++ OOP/decorators.py
@@ -1,22 +1,25 @@
 # Decorators
 
 def func(f):
-	def wrapper(*args, **kwargs): # to pass arguments
-		print('Started')
-		rv = f(*args, **kwargs) # to be able to return a variable
-		print('Ended')
-		return rv
+    def wrapper(*args, **kwargs):  # to pass arguments
+        print('Started')
+        rv = f(*args, **kwargs)  # to be able to return a variable
+        print('Ended')
+        return rv
 
-	return wrapper
+    return wrapper
+
 
 @func
 def func2(x):
-	print('i am func2', x)
-	return x
+    print('i am func2', x)
+    return x
+
 
 @func
 def func3():
-	print('i am func3')
+    print('i am func3')
+
 
 x = func2(5)
 print(x)
@@ -26,24 +29,29 @@ func3()
 ''' Timer decorator '''
 import time
 
+
 def timer(func):
-	def wrapper(*args, **kwargs):
-		start = time.time()
-		rv = func()
-		total = time.time() - start
-		print('Time:', total)
-		return rv
-	return wrapper
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        rv = func()
+        total = time.time() - start
+        print('Time:', total)
+        return rv
+
+    return wrapper
+
 
 @timer
 def test():
-	for _ in range(10000000):
-		# print(_)
-		pass
+    for _ in range(10000000):
+        # print(_)
+        pass
+
 
 @timer
 def test2():
-	time.sleep(2)
+    time.sleep(2)
+
 
 test()
 
