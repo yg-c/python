4ebc392e395f431f67bfb70cb7a252b480176f18
Yann <yann.cla@gmail.com>
2023-11-10 16:45:09 +0100

    Update inheritance.py

--- OOP/inheritance.py
+++ OOP/inheritance.py
@@ -1,37 +1,36 @@
 # base inheritance
 
 class Pet:
-	def __init__(self, name, age):
-		self.name = name
-		self.age = age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-	def show(self):
-		print(f'I am {self.name} and I am {self.age} years old')
+    def show(self):
+        print(f'I am {self.name} and I am {self.age} years old')
 
-	def speak(self):
-		print('no speak')
+    def speak(self):
+        print('no speak')
 
 
 class Cat(Pet):
-	def __init__(self, name, age, color):
-		super().__init__(name, age) # récupérer name age de Pet, call the super class
-		self.color = color
+    def __init__(self, name, age, color):
+        super().__init__(name, age)  # récupérer name age de Pet, call the super class
+        self.color = color
 
+    def speak(self):
+        print('Meow')
 
-	def speak(self):
-		print('Meow')
-
-	def show(self):
-		print(f'I am {self.name} and I am {self.age} years old and I am {self.color}')
+    def show(self):
+        print(f'I am {self.name} and I am {self.age} years old and I am {self.color}')
 
 
 class Dog(Pet):
-	def speak(self):
-		print('Bark')
+    def speak(self):
+        print('Bark')
 
 
 class Fish(Pet):
-	pass
+    pass
 
 
 p = Pet('Tim', 19)
